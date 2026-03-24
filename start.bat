c1b462bf765e9b37796eae01357944a815bdddd1
DESKTOP-RGVQ7E8\owner <andy.croon@gmail.com>
2026-03-24 03:20:37 +0100

    fix: start.bat sets own working dir so it works when called from anywhere

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
 @echo off
+cd /d "%~dp0"
 if not exist "venv\Scripts\activate.bat" (
     echo Virtual environment not found. Run install steps from README first.
+    pause
     exit /b 1
 )
 call venv\Scripts\activate.bat
